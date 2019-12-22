4738369a47ad23c48568a3c7171e095bd8421054
rhill34 <rhill34@mail.greenriver.edu>
2019-12-21 23:31:57 -0800

    :sparkles: Added function to get a selected note. @rhill34

---
+++ notes-app-api/get.js
@@ -0,0 +1,33 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+// This follows exactly the same structure as our previous function.
+//The major difference here is what we are doing a dynamoDbLib.call('get', params) to get a note object given notesId and userId that is passed in through the request.
+export async function main(event, context) {
+    const params = {
+        TableName: process.env.tableName,
+        // 'Key' defines the partition key and sort key of the item to be retrieved
+        // - 'userId': Identity Pool identity id of the authenticated user
+        // - 'noteId': path parameter
+        Key: {
+            userId:
+                event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+    try {
+        const result = await dynamoDbLib.call("get",
+            params);
+        if (result.Item) {
+            // Return the retrieved item
+            return success(result.Item);
+        } else {
+            return failure({
+                status: false, error: "Item not found."
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+    }
+}
