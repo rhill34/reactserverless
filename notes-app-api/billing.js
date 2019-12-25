8b87252fb54b31054d8a8ea310f5b89a22df0671
rhill34 <rhill34@mail.greenriver.edu>
2019-12-25 02:09:50 -0600

    :sparkles: Functionality test for the billing.js serverless.yml and Mocks/. @rhill34

--- notes-app-api/billing.js
+++ notes-app-api/billing.js
@@ -3,8 +3,7 @@ import { calculateCost } from "./libs/billing-lib"; import { success, failure } 
 export async function main(event, context) {
     const { storage, source } = JSON.parse(event.body); const amount = calculateCost(storage);
     const description = "Scratch charge";
-    // Load our secret key from the  environment
-    variables
+    // Load our secret key from the  environment variables
     const stripe = stripePackage(process.env.stripeSecretKey);
     try {
         await stripe.charges.create({
