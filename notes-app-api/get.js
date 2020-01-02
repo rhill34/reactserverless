71f728bb98f8aea67507827b81bc8b88e135c0cd
rhill34 <rhill34@mail.greenriver.edu>
2020-01-02 13:10:25 -0600

    Debug issue display note page, found and fixed. @rhill34
    Used none matching variables from API to Client.

--- notes-app-api/get.js
+++ notes-app-api/get.js
@@ -16,8 +16,7 @@ export async function main(event, context) {
         }
     };
     try {
-        const result = await dynamoDbLib.call("get",
-            params);
+        const result = await dynamoDbLib.call("get", params);
         if (result.Item) {
             // Return the retrieved item
             return success(result.Item);
